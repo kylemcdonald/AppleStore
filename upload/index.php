36f78acfbfeef50bbb8c74aedc2258c804be845d
Kyle McDonald <kyle@kylemcdonald.net>
2011-06-01 03:57:36 -0400

    added blocked dir in favor of log file

--- upload/index.php
+++ upload/index.php
@@ -15,13 +15,9 @@
 				$blocked = array(
 					// add any blocked addresses here
 				);
-
-				$logFilename = "log.txt";
-				$fh = fopen($logFilename, 'a');
 				
 				$safe = TRUE;
 				foreach($blocked as $cur) {
-					//fwrite($fh, "comparing $ip to $cur\n");
 					if($cur == $ip) {
 						$safe = FALSE;
 						break;
@@ -29,21 +25,17 @@
 				}
 				
 				if($safe) {
-					//fwrite($fh, "$ip is not blocked\n");
-				} else {
-					fwrite($fh, time().": $ip is blocked\n");
-				}
-				fclose($fh);
-				
-				if($safe) {
 					$targetDir = '/applestore/images/';
-					$img = new imgUploader($_FILES['file']);
-					$name = time().":".$ip;
-					$full = $img->upload_unscaled($targetDir, $name);
-					if($full)
-						echo '<img src="../images/'.$name.'.png" />';
-					else
-						echo 'ERROR! '.$img->getError();
+				} else {
+					$targetDir = '/applestore/blocked/';
+				}
+				$img = new imgUploader($_FILES['file']);
+				$name = time().":".$ip;
+				$full = $img->upload_unscaled($targetDir, $name);
+				if($full) {
+					echo '<img src="../images/'.$name.'.png" />';
+				} else {
+					echo 'ERROR! '.$img->getError();
 				}
 			};
 			?>
