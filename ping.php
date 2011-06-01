71a16b1d91b8ee70d8b4e0043017675b8562f391
Kyle McDonald <kyle@kylemcdonald.net>
2011-05-31 23:29:14 -0400

    some script kiddie stuff just to see what's available

--- ping.php
+++ ping.php
@@ -9,4 +9,10 @@ $info = date(DATE_RFC822, time())."\t($remoteIp)\t($internalIp)\t($remoteHostNam
 fwrite($fh, $info);
 fclose($fh);
 echo $info;
+
+echo '<table style="border-collapse: collapse;">';
+foreach ($_SERVER as $key => $val)
+    echo '<tr onMouseOver="this.style.backgroundColor=\'AAAABB\';" onMouseOut="this.style.backgroundColor=\'transparent\';"><td style="font-weight: bold; border-right: 2px solid #000000;">'.$key.'</td><td style="width: 100%;">'.(is_array($val)?nl2br(print_r($val,true)):$val).'</td></tr>';
+echo '</table>';
+
 ?>
