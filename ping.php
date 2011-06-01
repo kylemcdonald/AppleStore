7d86a0845c84d2d6902dd40b03b38d725bfed202
Kyle McDonald <kyle@kylemcdonald.net>
2011-05-31 23:35:29 -0400

    just printing remote port

--- ping.php
+++ ping.php
@@ -3,16 +3,9 @@ date_default_timezone_set('UTC');
 $logFilename = "ping.txt";
 $fh = fopen($logFilename, 'a');
 $remoteIp = $_SERVER['REMOTE_ADDR'];
-$internalIp = $_SERVER['HTTP_X_FORWARDED_FOR'];
-$remoteHostName = $_SERVER['REMOTE_HOST'];
-$info = date(DATE_RFC822, time())."\t($remoteIp)\t($internalIp)\t($remoteHostName)\n";
+$remotePort = $_SERVER['REMOTE_PORT'];
+$info = date(DATE_RFC822, time())."\t$remoteIp\t$remotePort\n";
 fwrite($fh, $info);
 fclose($fh);
 echo $info;
-
-echo '<table style="border-collapse: collapse;">';
-foreach ($_SERVER as $key => $val)
-    echo '<tr onMouseOver="this.style.backgroundColor=\'AAAABB\';" onMouseOut="this.style.backgroundColor=\'transparent\';"><td style="font-weight: bold; border-right: 2px solid #000000;">'.$key.'</td><td style="width: 100%;">'.(is_array($val)?nl2br(print_r($val,true)):$val).'</td></tr>';
-echo '</table>';
-
 ?>
