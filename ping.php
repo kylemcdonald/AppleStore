e9425e0bb3ac111182a3ee0bdc8f9dff3c85bf2c
Kyle McDonald <kyle@kylemcdonald.net>
2011-05-31 23:27:05 -0400

    more internalIp testing

--- ping.php
+++ ping.php
@@ -5,6 +5,8 @@ $fh = fopen($logFilename, 'a');
 $remoteIp = $_SERVER['REMOTE_ADDR'];
 $internalIp = $_SERVER['HTTP_X_FORWARDED_FOR'];
 $remoteHostName = $_SERVER['REMOTE_HOST'];
-fwrite($fh, date(time())."\t$remoteIp\t$internalIp\t$remoteHostName\n");
+$info = date(DATE_RFC822, time())."\t($remoteIp)\t($internalIp)\t($remoteHostName)\n";
+fwrite($fh, $info);
 fclose($fh);
+echo $info;
 ?>
