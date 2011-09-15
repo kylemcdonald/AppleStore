8af1c0b41ecdfafe6bd615e84b62150bf3a14578
Kyle McDonald <kyle@kylemcdonald.net>
2011-09-15 21:22:55 +0900

    updated to match main page

--- index.php
+++ index.php
@@ -3,11 +3,18 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>People Staring at Computers</title>
+		<link rel="stylesheet" href="../style.css" type="text/css"/>
 		<link rel="stylesheet" href="style.css" type="text/css"/>
 	</head>
 	<body>
-		<p><span class="header"><a href="http://kylemcdonald.net/psac/">People Staring at Computers</a></span> was a photographic intervention by <a href="http://kylemcdonald.net/">Kyle McDonald</a> that automatically posts pictures of people in computer stores around NYC. A custom app was used that took a picture each minute and uploaded it. Photos were then exhibited on siteg b, full screen, on every computer.</p>
+		<div class="section">
+		<div class="title"><h2>People Staring at Computers</h2></div>
 
+		<div class="cell">
+			<div class="description">
+			<h3>People Staring at Computers</h3> (<em>2011</em>) was a photographic intervention by <a href="http://kylemcdonald.net/">Kyle McDonald</a> that automatically posted pictures of people in computer stores around NYC. A custom app was used that took a picture each minute and uploaded it. Photos were then exhibited on site, full screen, on every computer.
+			</div>
+			<ul><li>
 <?php
 $dir = opendir("images");
 while($cur = readdir($dir)) {
@@ -31,8 +38,11 @@ if(!$perPage) {
 	$perPage = 120;
 }
 
-print("\t\t<p class=\"info\">$n people are staring at computers.</p>\n");
-
+print("$n people are staring at computers.");
+?>			
+			</li></ul>
+		</div>
+<?php
 // pagination could be cleaner, it's weird because i was doing it very wrong before
 for($i = 0; $i < $perPage;) {
 		if($cur < $n) {
@@ -52,10 +62,11 @@ for($i = 0; $i < $perPage;) {
 if($cur < $n) {
 		$nextPage = 1 + intval(ceil($cur / $perPage));
 		$total = intval(ceil($n / $perPage));
-		print("\t\t<a href=\"?start=$cur\"><p>Click here for page $nextPage of $total</p></a>");
+		print("\t\t<div class="cell"><a href=\"?start=$cur\"><p>Click here for page $nextPage of $total</p></a></div>");
 }
 
 ?>
+		</div>
 
 
 <!-- Start of StatCounter Code -->
